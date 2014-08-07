b4398d473f9e150146677467f526b21b9b40189b
s-giuliani <contact@alternetive.fr>
2014-08-07 16:38:45 +0200

    Mise à jour Interface

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 	require("sources/config/app.config.php");
 	require("sources/config/app.parametres.php");
 	
+	/* Fonction de construction du menu */
+	function item_menu($keywordhref, $label, $cssclass){
+		echo "<li class=\"".$cssclass."\"><a href=\"".$keywordhref."\">".$label."</a></li>\n";
+		}
+
+
+
 	if(isset($_GET["menu"]))
 	{
 		$menu = $_GET["menu"];
