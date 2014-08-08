298e24999a50dd4383f4e21e8f537102fa340b4f
sarahvoyance <soureh88@hotmail.com>
2014-08-08 11:16:36 +0200

    message

---
+++ models/commande.class.php
@@ -0,0 +1,10 @@
+<?php
+require_once('product.class.php');
+class Commande extends Product
+{
+	public function __construct()
+	{
+		echo "Commande<br/>";
+		echo $this->get
+	}
+}
---
+++ models/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'product.class.php';
+$product = new Product();
+/*
+(isset($_POST['login'], $_POST['password'], $_POST['email'], $_POST['birthdate']))
+{
+	require('motos.class.php');
+	$user = new User($_POST['login'], $_POST['password'], $_POST['email'], $_POST['birthdate']);
+	require('user.phtml');
+}
+else
+	require('form.phtml');*/
