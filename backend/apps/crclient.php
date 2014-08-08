68a294bdfc88295d104ab36bfb1eba77840e9f6d
mma75 <mmabbad@live.fr>
2014-08-08 15:48:33 +0200

    erevan22 - client

--- backend/apps/crclient.php
+++ backend/apps/crclient.php
@@ -1,4 +1,15 @@
 <?php
-include("views/crclient.phtml");
+require("../models/class.client.php");
+
+
+if ((isset($_POST['enrclt'])))
+
+{
+	$client = new Client($_POST); // instance nouveau client /
+	if ($client->isOK())
+			echo "contrôle client ok";
+}
+
+require("views/crclient.phtml");
 ?>
 
