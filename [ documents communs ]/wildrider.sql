dd35e46775a2b85cd233e9af8a7b365a79ccdf72
mma75 <mmabbad@live.fr>
2014-08-11 11:34:37 +0200

    erevan22 10.8.14

--- [ documents communs ]/wildrider.sql
+++ [ documents communs ]/wildrider.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 11, 2014 at 09:57 AM
+-- Generation Time: Aug 11, 2014 at 11:33 AM
 -- Server version: 5.5.37-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4
 
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `t_adresse`
 --
 
+DROP TABLE IF EXISTS `t_adresse`;
 CREATE TABLE IF NOT EXISTS `t_adresse` (
   `id_adresse` int(11) NOT NULL AUTO_INCREMENT,
   `id_client` int(11) NOT NULL,
@@ -34,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `t_adresse` (
   `cp` varchar(5) COLLATE utf8_bin NOT NULL,
   `ville` varchar(70) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_adresse`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -42,11 +43,12 @@ CREATE TABLE IF NOT EXISTS `t_adresse` (
 -- Table structure for table `t_categorie`
 --
 
+DROP TABLE IF EXISTS `t_categorie`;
 CREATE TABLE IF NOT EXISTS `t_categorie` (
   `id_cat` int(11) NOT NULL AUTO_INCREMENT,
   `desc` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_cat`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -54,6 +56,7 @@ CREATE TABLE IF NOT EXISTS `t_categorie` (
 -- Table structure for table `t_client`
 --
 
+DROP TABLE IF EXISTS `t_client`;
 CREATE TABLE IF NOT EXISTS `t_client` (
   `id_client` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -62,19 +65,20 @@ CREATE TABLE IF NOT EXISTS `t_client` (
   `prenom` varchar(50) COLLATE utf8_bin NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `telephone` varchar(10) COLLATE utf8_bin NOT NULL,
+  `refcli` varchar(16) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_client`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `t_client`
 --
 
-INSERT INTO `t_client` (`id_client`, `nom`, `login`, `password`, `prenom`, `email`, `telephone`) VALUES
-(1, 'Motoracer', 'user1', '698d51a19d8a121ce581499d7b701668', 'Antoine', 'antoine@aaa.fr', '0606060606'),
-(2, 'Bycle', 'user2', '698d51a19d8a121ce581499d7b701668', 'Baba', 'antoine@aaa.fr', '0606060606'),
-(3, 'Ghostrider', 'user3', '698d51a19d8a121ce581499d7b701668', 'Juju', 'juju@aaa.fr', '0606060606'),
-(4, 'Lebolide', 'user4', '698d51a19d8a121ce581499d7b701668', 'Jean', 'jean@aaa.fr', '0606060606'),
-(5, 'Lefouduvolant', 'user5', '698d51a19d8a121ce581499d7b701668', 'Chacha', 'chacha@aaa.fr', '0606060606');
+INSERT INTO `t_client` (`id_client`, `nom`, `login`, `password`, `prenom`, `email`, `telephone`, `refcli`) VALUES
+(1, 'Motoracer', 'user1', '698d51a19d8a121ce581499d7b701668', 'Antoine', 'antoine@aaa.fr', '0606060606', ''),
+(2, 'Bycle', 'user2', '698d51a19d8a121ce581499d7b701668', 'Baba', 'antoine@aaa.fr', '0606060606', ''),
+(3, 'Ghostrider', 'user3', '698d51a19d8a121ce581499d7b701668', 'Juju', 'juju@aaa.fr', '0606060606', ''),
+(4, 'Lebolide', 'user4', '698d51a19d8a121ce581499d7b701668', 'Jean', 'jean@aaa.fr', '0606060606', ''),
+(5, 'Lefouduvolant', 'user5', '698d51a19d8a121ce581499d7b701668', 'Chacha', 'chacha@aaa.fr', '0606060606', '');
 
 -- --------------------------------------------------------
 
@@ -82,6 +86,7 @@ INSERT INTO `t_client` (`id_client`, `nom`, `login`, `password`, `prenom`, `emai
 -- Table structure for table `t_commande`
 --
 
+DROP TABLE IF EXISTS `t_commande`;
 CREATE TABLE IF NOT EXISTS `t_commande` (
   `id_commande` int(11) NOT NULL AUTO_INCREMENT,
   `id_client` int(11) NOT NULL,
@@ -89,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `t_commande` (
   `datecde` date NOT NULL,
   `etat` varchar(1) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_commande`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -97,11 +102,12 @@ CREATE TABLE IF NOT EXISTS `t_commande` (
 -- Table structure for table `t_panier`
 --
 
+DROP TABLE IF EXISTS `t_panier`;
 CREATE TABLE IF NOT EXISTS `t_panier` (
   `id_panier` int(11) NOT NULL AUTO_INCREMENT,
   `id_client` int(11) NOT NULL,
   PRIMARY KEY (`id_panier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -109,6 +115,7 @@ CREATE TABLE IF NOT EXISTS `t_panier` (
 -- Table structure for table `t_produit`
 --
 
+DROP TABLE IF EXISTS `t_produit`;
 CREATE TABLE IF NOT EXISTS `t_produit` (
   `id_produit` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id produit',
   `nom` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -118,22 +125,23 @@ CREATE TABLE IF NOT EXISTS `t_produit` (
   `sscategorie` smallint(6) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_bin NOT NULL,
   `image` varchar(255) COLLATE utf8_bin NOT NULL,
+  `refpdt` varchar(16) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_produit`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `t_produit`
 --
 
-INSERT INTO `t_produit` (`id_produit`, `nom`, `desc`, `prix`, `categorie`, `sscategorie`, `couleur`, `image`) VALUES
-(1, 'MOTO1', 'SUPER MOTO 1', 3000, 0, 0, 'Gris', '1'),
-(2, 'MOTO2', 'SUPER MOTO 2', 6000, 0, 0, 'Rouge', '2'),
-(3, 'MOTO3', 'SUPER MOTO 3', 6000, 0, 0, 'Blanc', '3'),
-(4, 'MOTO4', 'SUPER MOTO 4', 6000, 0, 0, 'Blanc', '4'),
-(5, 'MOTO5', 'SUPER MOTO 4', 6000, 0, 0, 'Vert', '5'),
-(6, 'MOTO5', 'SUPER MOTO 6', 6000, 0, 0, 'Marron', '6'),
-(7, 'MOTO7', 'SUPER MOTO 6', 6000, 0, 0, 'Noir', '7'),
-(8, 'MOTO7', 'SUPER MOTO 6', 6000, 0, 0, 'Jaune', '8');
+INSERT INTO `t_produit` (`id_produit`, `nom`, `desc`, `prix`, `categorie`, `sscategorie`, `couleur`, `image`, `refpdt`) VALUES
+(1, 'MOTO1', 'SUPER MOTO 1', 3000, 0, 0, 'Gris', '1', ''),
+(2, 'MOTO2', 'SUPER MOTO 2', 6000, 0, 0, 'Rouge', '2', ''),
+(3, 'MOTO3', 'SUPER MOTO 3', 6000, 0, 0, 'Blanc', '3', ''),
+(4, 'MOTO4', 'SUPER MOTO 4', 6000, 0, 0, 'Blanc', '4', ''),
+(5, 'MOTO5', 'SUPER MOTO 4', 6000, 0, 0, 'Vert', '5', ''),
+(6, 'MOTO5', 'SUPER MOTO 6', 6000, 0, 0, 'Marron', '6', ''),
+(7, 'MOTO7', 'SUPER MOTO 6', 6000, 0, 0, 'Noir', '7', ''),
+(8, 'MOTO7', 'SUPER MOTO 6', 6000, 0, 0, 'Jaune', '8', '');
 
 -- --------------------------------------------------------
 
@@ -141,12 +149,13 @@ INSERT INTO `t_produit` (`id_produit`, `nom`, `desc`, `prix`, `categorie`, `ssca
 -- Table structure for table `t_sscat`
 --
 
+DROP TABLE IF EXISTS `t_sscat`;
 CREATE TABLE IF NOT EXISTS `t_sscat` (
   `id_sscat` int(11) NOT NULL AUTO_INCREMENT,
   `desc` varchar(255) COLLATE utf8_bin NOT NULL,
   `id_cat` int(11) NOT NULL,
   PRIMARY KEY (`id_sscat`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -154,6 +163,7 @@ CREATE TABLE IF NOT EXISTS `t_sscat` (
 -- Table structure for table `t_user`
 --
 
+DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE IF NOT EXISTS `t_user` (
   `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(32) COLLATE utf8_bin NOT NULL,
