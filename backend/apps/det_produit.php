f83078366fe41b71905dc81694b2521750d75644
mma75 <mmabbad@live.fr>
2014-08-11 14:42:47 +0200

    erevan22 10.8.14

--- backend/apps/det_produit.php
+++ backend/apps/det_produit.php
@@ -1,6 +1,20 @@
 <?php
 
-var_dump($_GET['produit']);
+$produit = $_GET['produit'];
+$produits = array();
+
+$db =  mysqli_connect("localhost","root","troiswa","wildrider");
+//Recherche du produit dans la base */
+
+
+$req = "SELECT * FROM t_produit where id_produit =". $produit;
+$res = mysqli_query($db, $req);
+while ($data = mysqli_fetch_assoc($res))
+{
+	$produits[] = $data;//* on récupère les infos du produit
+}
+
 
 include("views/det_produit.phtml");
+
 ?>
